0ced5cfc0fbf2dddca751602e8ef0c283310a248
jose luis sanchez romero <jose.luis.sanchez.romero@ing.com>
2024-01-26 21:54:22 +0100

    Upgraded to ollama 0.71

--- src/main/java/org/sebi/IngestorExample.java
+++ src/main/java/org/sebi/IngestorExample.java
@@ -2,12 +2,17 @@ package org.sebi;
 import static dev.langchain4j.data.document.splitter.DocumentSplitters.recursive;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.List;
 
 import dev.langchain4j.data.document.Document;
-import dev.langchain4j.data.document.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import io.quarkiverse.langchain4j.ollama.OllamaChatLanguageModel;
+import io.quarkiverse.langchain4j.ollama.OllamaEmbeddingModel;
 import io.quarkiverse.langchain4j.pgvector.PgVectorEmbeddingStore;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -27,12 +32,19 @@ public class IngestorExample {
      * The embedding model (how the vector of a document is computed).
      * The bean is provided by the LLM (like openai) extension.
      */
-    @Inject
     EmbeddingModel embeddingModel;
 
-    public void ingest(@Observes StartupEvent event) {
+  public IngestorExample() {
+    embeddingModel = OllamaEmbeddingModel.builder()
+        .model("phi")
+        .timeout(Duration.ofMinutes(2))
+        .build();
+  }
+
+  public void ingest(@Observes StartupEvent event) {
         System.out.printf("Ingesting documents...%n");
-        List<Document> documents = FileSystemDocumentLoader.loadDocuments(new File("src/main/resources/catalog").toPath());
+        var path = new File("src/main/resources/catalog").toPath();
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(path, new TextDocumentParser(StandardCharsets.UTF_8));
         var ingestor = EmbeddingStoreIngestor.builder()
                 .embeddingStore(store)
                 .embeddingModel(embeddingModel)
--- src/main/java/org/sebi/RetrieverResource.java
+++ src/main/java/org/sebi/RetrieverResource.java
@@ -1,11 +1,13 @@
 package org.sebi;
 
+import java.time.Duration;
 import java.util.List;
 
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.retriever.EmbeddingStoreRetriever;
 import dev.langchain4j.retriever.Retriever;
+import io.quarkiverse.langchain4j.ollama.OllamaEmbeddingModel;
 import io.quarkiverse.langchain4j.pgvector.PgVectorEmbeddingStore;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -14,8 +16,12 @@ import jakarta.ws.rs.Path;
 public class RetrieverResource {
      private final EmbeddingStoreRetriever retriever;
 
-    RetrieverResource(PgVectorEmbeddingStore store, EmbeddingModel model) {
-        retriever = EmbeddingStoreRetriever.from(store, model, 20);
+    RetrieverResource(PgVectorEmbeddingStore store) {
+        EmbeddingModel embeddingModel = OllamaEmbeddingModel.builder()
+            .model("phi")
+            .timeout(Duration.ofMinutes(2))
+            .build();
+        retriever = EmbeddingStoreRetriever.from(store, embeddingModel, 20);
     }
 
     @GET
